fb730906049fc56db2b1662e4a35b79250afba28
Prakruthi-G26 <prakruthig9437@gmail.com>
2025-03-13 12:37:43 +0530

    my first commit

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add{
+    public static void main(String args[]){
+        int n1=20;
+        int n2=30;
+        int sum;
+        sum=n1+n2;
+        System.out.println("the sum of two number is: "+sum);
+    }
+}
